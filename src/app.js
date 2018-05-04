52a7ea47586a915776d7fcb1eafa1b17de352fe0
Murali888999 <mohanggk24@gmail.com>
2018-05-04 21:26:41 +0530

    webpack

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import "./styles/styles.scss";
+// import {add as ahello,sub as shello,mul as mhello,div as dhello} from "./script.js";
+// import anything from "./script.js";
+
+// console.log(ahello(40,200));
+
+// console.log(shello(400,20));
+
+// console.log(mhello(20,400));
+
+// console.log(dhello(200,40));
+// anything("Murali");
+// const arrowfunction =(name) => name;
+// console.log(arrowfunction("Murali"));
---
+++ src/script.js
@@ -0,0 +1,28 @@
+var add = function(a,b)
+{
+    return a + b;
+}
+console.log(add(9,4));
+
+var sub = function(a,b)
+{
+    return a - b;
+}
+console.log(add(9,4));
+
+var mul = function(a,b)
+{
+    return a * b;
+}
+console.log(add(9,4));
+
+var div = function(a,b)
+{
+    return a / b;
+}
+console.log(add(9,4));
+var printName =function(name){
+    console.log(name);
+}
+export default printName;
+export {add,sub,mul,div};
